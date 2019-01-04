1515d5d66c2a98e6fc59a6a4830272a91ffaae1d
George Lucas T. Bentes <george.bentes@gmail.com>
2019-01-03 22:49:13 -0400

    Redefining Architecture

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h1>Readable App</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-12 col-md-8">
+
+          </div>
+          <div className="col-sm-12 col-md-4">
+
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
