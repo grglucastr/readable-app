0e681a53756d0e2fb41cdb39cccff6316051b2ad
George Lucas T. Bentes <george.bentes@gmail.com>
2019-01-23 06:00:25 -0400

    SortContentList UI

--- src/components/SortContentList.js
+++ src/components/SortContentList.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const SortContentList = () => (
   <div>
-    Sort Content List Component here
+    Sort:
+    <select style={{marginLeft: '5px'}}>
+      <option value="newest">Newest</option>
+      <option value="oldest" >Oldest</option>
+      <option value="most_voted" >Most voted</option>
+      <option value="less_voted">Less voted</option>
+    </select>
+    
   </div>
 )
 
